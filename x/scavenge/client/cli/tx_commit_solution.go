51dccb1836407629a44eeba8c45c986c79173727
SpiralOutDotEu <spiralout.eu@gmail.com>
2022-06-13 20:40:53 +0300

    modify CLI so that it sends hashes instead

--- x/scavenge/client/cli/tx_commit_solution.go
+++ x/scavenge/client/cli/tx_commit_solution.go
@@ -1,6 +1,8 @@
 package cli
 
 import (
+	"crypto/sha256"
+    "encoding/hex"
 	"strconv"
 
 	"github.com/SpiralOutDotEu/scavenge/x/scavenge/types"
@@ -18,27 +20,31 @@ func CmdCommitSolution() *cobra.Command {
 		Short: "Broadcast message commit-solution",
 		Args:  cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
-			argSolutionHash := args[0]
-			argSolutionScavengerHash := args[1]
-
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
 			}
 
-			msg := types.NewMsgCommitSolution(
-				clientCtx.GetFromAddress().String(),
-				argSolutionHash,
-				argSolutionScavengerHash,
-			)
+			solution := args[0]
+			// find a hash of the solution
+			solutionHash := sha256.Sum256([]byte(solution))
+			// convert the solution hash to string
+			solutionHashString := hex.EncodeToString(solutionHash[:])
+			// convert a scavenger address to string
+			var scavenger = clientCtx.GetFromAddress().String()
+			// find the hash of solution and scavenger address
+			var solutionScavengerHash = sha256.Sum256([]byte(solution + scavenger))
+			// convert the hash to string
+			var solutionScavengerHashString = hex.EncodeToString(solutionScavengerHash[:])
+			// create a new message
+			msg := types.NewMsgCommitSolution(clientCtx.GetFromAddress().String(), string(solutionHashString), string(solutionScavengerHashString))
 			if err := msg.ValidateBasic(); err != nil {
 				return err
 			}
+			// broadcast the transaction with the message to the blockchain
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
 		},
 	}
-
 	flags.AddTxFlagsToCmd(cmd)
-
 	return cmd
 }
